7629a4c79f33783067b9218fe8bcc5918327d199
Ivan <xvanok@gmail.com>
2023-08-24 15:57:23 +0500

    Update Vkontakte.php

--- src/Vkontakte.php
+++ src/Vkontakte.php
@@ -11,7 +11,7 @@ class Vkontakte extends AbstractProvider
 {
     protected $baseOAuthUri = 'https://oauth.vk.com';
     protected $baseUri      = 'https://api.vk.com/method';
-    protected $version      = '5.52';
+    protected $version      = '5.130';
     protected $language     = null;
 
     /**
